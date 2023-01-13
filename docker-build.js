2bb1c2f14e601b8ac007210be354f2ba64d9adbb
Glitch (mousy-phrygian-ton) <none>
2023-01-13 00:54:41 +0000

    🍝🍞 Checkpoint ./static/index.html:52075789/20 ./docker-build.js:52075789/503

---
+++ docker-build.js
@@ -0,0 +1,118 @@
+
+var correctuser = "bob"
+var correctpass = "sus"
+var correct = false
+while ( correct === false){
+  let user = prompt("Username");
+  let pass = prompt("Password");
+  if (user === "bob" && pass==="sus"){
+    correct = true;
+  } else {
+    alert("Wrong password or username")
+    correct = true;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
